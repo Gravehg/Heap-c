33e90a7c0d531a699d2db4a310b3d2374ac7d63a
Gravehg <80481681+Gravehg@users.noreply.github.com>
2024-07-13 18:43:29 -0600

    Created Heap.h, added structs and function definitions

---
+++ Heap.h
@@ -0,0 +1,36 @@
+#ifndef HEAP_H
+#define HEAP_H
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef enum
+{
+    MINHEAP,
+    MAXHEAP,
+} TYPE_HEAP;
+
+typedef struct Heap
+{
+    TYPE_HEAP type;
+    int *heap_elements;
+    int max_size;
+    int size;
+} Heap;
+
+Heap *get_heap(TYPE_HEAP selected_type, int max_size);
+int get_max(Heap *heap);
+int get_min(Heap *heap);
+int right_child_heap(Heap *heap, int i);
+int left_child_heap(Heap *heap, int i);
+int parent_heap(Heap *heap, int i);
+int greater_child_heap(Heap *heap, int i);
+void insert_heap(Heap *heap, int element);
+void remove_heap(Heap *heap, int element);
+int is_leaf_heap(Heap *heap, int i);
+int get_size_heap(Heap *heap);
+int is_empty_heap(Heap *heap);
+int sift_down_heap(Heap *heap);
+int sift_up_heap(Heap *heap);
+int remove_heap(Heap *heap);
+
+#endif
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Heap-C
+
+A basic implementation of a heap (Min or Max) using the C programming language.
